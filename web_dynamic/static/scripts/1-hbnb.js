d4f5479e1354aa65ee488361123351cb8205c177
ismailouzy <ismailouzal@gmail.com>
2024-06-02 07:31:45 -0700

    d

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    const selectedAmenities = {};
+  
+    $('input[type="checkbox"]').change(function () {
+      const amenityId = $(this).data('id');
+      const amenityName = $(this).data('name');
+  
+      if (this.checked) {
+        selectedAmenities[amenityId] = amenityName;
+      } else {
+        delete selectedAmenities[amenityId];
+      }
+  
+      const amenityList = Object.values(selectedAmenities).join(', ');
+      $('div.amenities h4').text(amenityList);
+    });
+  });
